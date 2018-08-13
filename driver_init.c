75837e84faccee875db9e0be109dc4977e27f385
wa1tnr <wa1tnr@users.noreply.github.com>
2018-08-13 21:02:25 +0000

    Named USART_5 to _0 in the online tool.  Good idea. 	modified:   driver_init.c Good idea to do it in the tool rather than manually. You have to create six USARTs to get to the TX/RX pair used on Feather M4 Express.  Once created, you can remove the others (they aren't wanted).
    Here, the USART_5 was renamed USART_0 as it's the first
    on the Feather M4 Express.
    On branch testing-bb

--- driver_init.c
+++ driver_init.c
@@ -11,9 +11,9 @@
 #include <utils.h>
 #include <hal_init.h>
 
-struct usart_sync_descriptor USART_5;
+struct usart_sync_descriptor USART_0;
 
-void USART_5_PORT_init(void)
+void USART_0_PORT_init(void)
 {
 
 	gpio_set_pin_function(PB16, PINMUX_PB16C_SERCOM5_PAD0);
@@ -21,7 +21,7 @@ void USART_5_PORT_init(void)
 	gpio_set_pin_function(PB17, PINMUX_PB17C_SERCOM5_PAD1);
 }
 
-void USART_5_CLOCK_init(void)
+void USART_0_CLOCK_init(void)
 {
 	hri_gclk_write_PCHCTRL_reg(GCLK, SERCOM5_GCLK_ID_CORE, CONF_GCLK_SERCOM5_CORE_SRC | (1 << GCLK_PCHCTRL_CHEN_Pos));
 	hri_gclk_write_PCHCTRL_reg(GCLK, SERCOM5_GCLK_ID_SLOW, CONF_GCLK_SERCOM5_SLOW_SRC | (1 << GCLK_PCHCTRL_CHEN_Pos));
@@ -29,11 +29,11 @@ void USART_5_CLOCK_init(void)
 	hri_mclk_set_APBDMASK_SERCOM5_bit(MCLK);
 }
 
-void USART_5_init(void)
+void USART_0_init(void)
 {
-	USART_5_CLOCK_init();
-	usart_sync_init(&USART_5, SERCOM5, (void *)NULL);
-	USART_5_PORT_init();
+	USART_0_CLOCK_init();
+	usart_sync_init(&USART_0, SERCOM5, (void *)NULL);
+	USART_0_PORT_init();
 }
 
 void system_init(void)
@@ -68,5 +68,5 @@ void system_init(void)
 
 	gpio_set_pin_function(D13, GPIO_PIN_FUNCTION_OFF);
 
-	USART_5_init();
+	USART_0_init();
 }
