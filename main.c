13719365850e65d1f6dc0d40ecf68989c8489db2
wa1tnr <wa1tnr@users.noreply.github.com>
2018-08-13 19:17:04 +0000

    Working USART demonstration 'hello world' from a new Atmel Start project. On branch testing-aa 	modified:   main.c First time this has worked, here in the lab.

--- main.c
+++ main.c
@@ -1,11 +1,40 @@
 #include <atmel_start.h>
+#include "driver_examples.h"
+#include "pins.h"
+
+void delays(void) { // delay some
+    for (volatile int i=1299999; i>0; i--) {
+        // empty loop
+    }
+}
+
+void blink_two(void) {
+    // clear first
+    PORT->Group[PORTA].OUTCLR.reg  = (uint32_t)(1 << 23); // PA23 //  0 13 pinwrite  // D13
+    for (int blinks=2; blinks >0; blinks--) {
+        PORT->Group[PORTA].OUTTGL.reg  = (uint32_t)(1 << 23); // PA23 //    13 pintoggle // D13
+        delays();
+    }
+}
+
+void blink_awhile(void) {
+    for (int iter=22; iter >0; iter--) {
+        blink_two();
+    }
+}
 
 int main(void)
 {
-	/* Initializes MCU, drivers and middleware */
-	atmel_start_init();
+    /* Initializes MCU, drivers and middleware */
+    atmel_start_init();
+    SystemInit();
+    pins_setup(); // initialize GPIO D13 PA23
 
-	/* Replace with your application code */
-	while (1) {
-	}
+    blink_awhile(); // is the clock running?
+
+    USART_5_example();
+    /* Replace with your application code */
+
+    while (1) {
+    }
 }
