18bc5fb5640459bc6249bf5265a8adf630bbb7d8
wa1tnr <wa1tnr@users.noreply.github.com>
2018-08-13 20:55:00 +0000

    Factory version - closed loop so needs manual edit for 48 MHz (is zero!) 	modified:   config/peripheral_clk_config.h If it's set open loop the 48 MHz is set; if closed you have to setup for 48 MHz manually.  That's the working theory here, anyway. ;) On branch testing-bb

--- config/peripheral_clk_config.h
+++ config/peripheral_clk_config.h
@@ -9,27 +9,11 @@
  * \brief CPU's Clock frequency
  */
 #ifndef CONF_CPU_FREQUENCY
-#define CONF_CPU_FREQUENCY 48000000
+#define CONF_CPU_FREQUENCY 0
 #endif
 
-// <y> RTC Clock Source
-// <id> rtc_clk_selection
-// <RTC_CLOCK_SOURCE"> RTC source
-// <i> Select the clock source for RTC.
-#ifndef CONF_GCLK_RTC_SRC
-#define CONF_GCLK_RTC_SRC RTC_CLOCK_SOURCE
-#endif
-
-/**
- * \def CONF_GCLK_RTC_FREQUENCY
- * \brief RTC's Clock frequency
- */
-#ifndef CONF_GCLK_RTC_FREQUENCY
-#define CONF_GCLK_RTC_FREQUENCY 32768
-#endif
-
-// <y> TC Clock Source
-// <id> tc_gclk_selection
+// <y> Core Clock Source
+// <id> core_gclk_selection
 
 // <GCLK_PCHCTRL_GEN_GCLK0_Val"> Generic clock generator 0
 
@@ -54,84 +38,38 @@
 // <GCLK_PCHCTRL_GEN_GCLK10_Val"> Generic clock generator 10
 
 // <GCLK_PCHCTRL_GEN_GCLK11_Val"> Generic clock generator 11
-
-// <i> Select the clock source for TC.
-#ifndef CONF_GCLK_TC0_SRC
-#define CONF_GCLK_TC0_SRC GCLK_PCHCTRL_GEN_GCLK1_Val
-#endif
-
-/**
- * \def CONF_GCLK_TC0_FREQUENCY
- * \brief TC0's Clock frequency
- */
-#ifndef CONF_GCLK_TC0_FREQUENCY
-#define CONF_GCLK_TC0_FREQUENCY 2000000
-#endif
-
-// <y> TC Clock Source
-// <id> tc_gclk_selection
-
-// <GCLK_PCHCTRL_GEN_GCLK0_Val"> Generic clock generator 0
-
-// <GCLK_PCHCTRL_GEN_GCLK1_Val"> Generic clock generator 1
-
-// <GCLK_PCHCTRL_GEN_GCLK2_Val"> Generic clock generator 2
-
-// <GCLK_PCHCTRL_GEN_GCLK3_Val"> Generic clock generator 3
-
-// <GCLK_PCHCTRL_GEN_GCLK4_Val"> Generic clock generator 4
-
-// <GCLK_PCHCTRL_GEN_GCLK5_Val"> Generic clock generator 5
-
-// <GCLK_PCHCTRL_GEN_GCLK6_Val"> Generic clock generator 6
-
-// <GCLK_PCHCTRL_GEN_GCLK7_Val"> Generic clock generator 7
-
-// <GCLK_PCHCTRL_GEN_GCLK8_Val"> Generic clock generator 8
-
-// <GCLK_PCHCTRL_GEN_GCLK9_Val"> Generic clock generator 9
-
-// <GCLK_PCHCTRL_GEN_GCLK10_Val"> Generic clock generator 10
-
-// <GCLK_PCHCTRL_GEN_GCLK11_Val"> Generic clock generator 11
-
-// <i> Select the clock source for TC.
-#ifndef CONF_GCLK_TC1_SRC
-#define CONF_GCLK_TC1_SRC GCLK_PCHCTRL_GEN_GCLK1_Val
-#endif
-
-/**
- * \def CONF_GCLK_TC1_FREQUENCY
- * \brief TC1's Clock frequency
- */
-#ifndef CONF_GCLK_TC1_FREQUENCY
-#define CONF_GCLK_TC1_FREQUENCY 2000000
-#endif
-
-
-// local local local
-
-/*
-   139  -MD -MP -MF "driver_init.d" -MT"driver_init.d" -MT"driver_init.o"  -o "driver_init.o" "../driver_init.c"
-   140  ../driver_init.c: In function 'USART_5_CLOCK_init':
-   141  ../driver_init.c:26:57: error: 'CONF_GCLK_SERCOM5_CORE_SRC' undeclared (first use in this function); did you mean 'CONF_GCLK_RTC_SRC'?
-   142    hri_gclk_write_PCHCTRL_reg(GCLK, SERCOM5_GCLK_ID_CORE, CONF_GCLK_SERCOM5_CORE_SRC | (1 << GCLK_PCHCTRL_CHEN_Pos));
-   143                                                           ^~~~~~~~~~~~~~~~~~~~~~~~~~
-   144                                                           CONF_GCLK_RTC_SRC
-   145  ../driver_init.c:26:57: note: each undeclared identifier is reported only once for each function it appears in
-   146  ../driver_init.c:27:57: error: 'CONF_GCLK_SERCOM5_SLOW_SRC' undeclared (first use in this function); did you mean 'CONF_GCLK_SERCOM5_CORE_SRC'?
-   147    hri_gclk_write_PCHCTRL_reg(GCLK, SERCOM5_GCLK_ID_SLOW, CONF_GCLK_SERCOM5_SLOW_SRC | (1 << GCLK_PCHCTRL_CHEN_Pos));
-   148                                                           ^~~~~~~~~~~~~~~~~~~~~~~~~~
-   149                                                           CONF_GCLK_SERCOM5_CORE_SRC
-   150  Makefile:180: recipe for target 'driver_init.o' failed
-
-*/
-
 
 // <i> Select the clock source for CORE.
 #ifndef CONF_GCLK_SERCOM5_CORE_SRC
 #define CONF_GCLK_SERCOM5_CORE_SRC GCLK_PCHCTRL_GEN_GCLK0_Val
 #endif
+
+// <y> Slow Clock Source
+// <id> slow_gclk_selection
+
+// <GCLK_PCHCTRL_GEN_GCLK0_Val"> Generic clock generator 0
+
+// <GCLK_PCHCTRL_GEN_GCLK1_Val"> Generic clock generator 1
+
+// <GCLK_PCHCTRL_GEN_GCLK2_Val"> Generic clock generator 2
+
+// <GCLK_PCHCTRL_GEN_GCLK3_Val"> Generic clock generator 3
+
+// <GCLK_PCHCTRL_GEN_GCLK4_Val"> Generic clock generator 4
+
+// <GCLK_PCHCTRL_GEN_GCLK5_Val"> Generic clock generator 5
+
+// <GCLK_PCHCTRL_GEN_GCLK6_Val"> Generic clock generator 6
+
+// <GCLK_PCHCTRL_GEN_GCLK7_Val"> Generic clock generator 7
+
+// <GCLK_PCHCTRL_GEN_GCLK8_Val"> Generic clock generator 8
+
+// <GCLK_PCHCTRL_GEN_GCLK9_Val"> Generic clock generator 9
+
+// <GCLK_PCHCTRL_GEN_GCLK10_Val"> Generic clock generator 10
+
+// <GCLK_PCHCTRL_GEN_GCLK11_Val"> Generic clock generator 11
 
 // <i> Select the slow clock source.
 #ifndef CONF_GCLK_SERCOM5_SLOW_SRC
@@ -143,7 +81,7 @@
  * \brief SERCOM5's Core Clock frequency
  */
 #ifndef CONF_GCLK_SERCOM5_CORE_FREQUENCY
-#define CONF_GCLK_SERCOM5_CORE_FREQUENCY 48000000
+#define CONF_GCLK_SERCOM5_CORE_FREQUENCY 0
 #endif
 
 /**
