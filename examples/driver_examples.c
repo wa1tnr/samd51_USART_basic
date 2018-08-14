760d5f21688bf9daf37728a07cd37977f9526b76
wa1tnr <wa1tnr@users.noreply.github.com>
2018-08-14 20:31:22 +0000

    TYPEWRITER is now available.  Well, it's a character echo. 	modified:   driver_examples.c On branch testing-gg-typewriter-

--- examples/driver_examples.c
+++ examples/driver_examples.c
@@ -10,14 +10,24 @@
 #include "driver_init.h"
 #include "utils.h"
 
+/* Terminal Input Buffer for interpreter */
+const uint8_t maxtib = 16;
+
 /**
  * Example of using USART_0 to write "Hello World" using the IO abstraction.
  */
 void USART_0_example(void)
 {
+	char tib[maxtib];
 	struct io_descriptor *io;
 	usart_sync_get_io_descriptor(&USART_0, &io);
 	usart_sync_enable(&USART_0);
 
-	io_write(io, (uint8_t *)"Hello World!", 12);
+	io_write(io, (uint8_t *)  "Hello World!\r\n",  14);
+	io_write(io, (uint8_t *)"  type something: ",  18);
+
+	while(-1) { // endless loop, read one char, write one char (echo)
+		io_read(io,  (uint8_t *)tib, 1); // 1  is length
+		io_write(io, (uint8_t *)tib, 1); // 1  is also length
+        }
 }
