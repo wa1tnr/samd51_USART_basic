ea295909b4dcd6a90ee00afa7070dccd12bca11c
wa1tnr <wa1tnr@users.noreply.github.com>
2018-08-14 20:58:18 +0000

    Somewhat encapsulated functions (to modularize the code a bit). 	modified:   ../examples/driver_examples.c On branch testing-gg-typewriter- Not proud of the names chosen for the wrappers. Wasn't expecting this and wanted to move on.
    TODO: figure this out.  Give them proper names.

--- examples/driver_examples.c
+++ examples/driver_examples.c
@@ -13,18 +13,38 @@
 /* Terminal Input Buffer for interpreter */
 const uint8_t maxtib = 16;
 
+struct io_descriptor *io;
+
+void usart_sync_get_desc(void) {
+    usart_sync_get_io_descriptor(&USART_0, &io);
+}
+
+void usart_sync_enbl(void) {
+    usart_sync_enable(&USART_0);
+}
+
+void fg_yellow(void) { // foreground yellow
+	io_write(io, (uint8_t *)"            ", 12);
+	io_write(io, (uint8_t *)"\033\133", 2); // ESC [
+	io_write(io, (uint8_t *)"\063\063", 2); // 33 - yellow fg
+	io_write(io, (uint8_t *)"m", 1);        // for the stanza
+}
+
 /**
  * Example of using USART_0 to write "Hello World" using the IO abstraction.
  */
 void USART_0_example(void)
 {
 	char tib[maxtib];
-	struct io_descriptor *io;
-	usart_sync_get_io_descriptor(&USART_0, &io);
-	usart_sync_enable(&USART_0);
+	// struct io_descriptor *io;
+	// usart_sync_get_io_descriptor(&USART_0, &io);
+        usart_sync_get_desc();
+	// usart_sync_enable(&USART_0);
+        usart_sync_enbl();
 
 	io_write(io, (uint8_t *)  "Hello World!\r\n",  14);
 	io_write(io, (uint8_t *)"  type something: ",  18);
+        fg_yellow(); // color it!
 
 	while(-1) { // endless loop, read one char, write one char (echo)
 		io_read(io,  (uint8_t *)tib, 1); // 1  is length
