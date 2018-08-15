b04ac1e5803bb02c83e6eeb8c062b0dcd0a797f4
wa1tnr <wa1tnr@users.noreply.github.com>
2018-08-15 06:17:47 +0000

    Warm boot mod.  Not much guarding against using it. 	modified:   driver_examples.c On branch testing-gg3-typewriter-

--- examples/driver_examples.c
+++ examples/driver_examples.c
@@ -1,6 +1,6 @@
 /* Code generated from Atmel Start - overwritten; do make a copy. */
 
-/* driver_examples.c */
+/* warm.c */
 
 /* wa1tnr - August, 2018 LGPL v2.1 */
 
@@ -35,7 +35,69 @@ void fg_yellow(void) { // foreground yellow
 char tib[maxtib];
 uint8_t *buf;
 
+int state_warm = 0;
+
+void _warm(void) { // reboot the machine
+    io_write(io, (uint8_t *)"  WARM BOOT \r\n", 14);
+    NVIC_SystemReset();      // processor software reset
+}
+
+void capture_warm(void) { // trap keystrokes to build a command
+
+/*
+    if (state_warm == -2) {
+        state_warm = -1; // demote double-penalty
+        return; // empty-handed - skip a turn
+    }
+
+*/
+
+    if ( (state_warm ==  0) && ((uint8_t) *buf != 0x77) ) { // ! 'w'
+        state_warm = -1;
+        return;
+    }
+
+
+    if ( (state_warm == 0) && ((uint8_t) *buf == 0x77) ) { // 'w'
+        // io_write(io, (uint8_t *) "!w!",  3);
+        state_warm = 0x77; // first level gained
+        return;
+    }
+
+
+    if ( (state_warm == 0x77) && ((uint8_t) *buf == 0x61) ) { // "wa..."
+        // io_write(io, (uint8_t *) "!a!",  3);
+        state_warm = 0x61; // second level gained
+        return;
+    }
+
+
+    if ( (state_warm == 0x61) && ((uint8_t) *buf == 0x72) ) {  // "war.."
+        // io_write(io, (uint8_t *) "!r!",  3);
+        state_warm = 0x72; // third level gained
+        return;
+    }
+
+
+    if ( (state_warm == 0x72) && ((uint8_t) *buf == 0x6d) ) {  // "warm."
+        io_write(io, (uint8_t *) "  !warm! ",  9);
+        state_warm = 0x6d; // fourth level gained
+        return;
+    }
+
+    if ( (state_warm == 0x6d) && ((uint8_t) *buf == 0x0d) ) {  // "warmCR"
+        io_write(io, (uint8_t *) " !reboot! ", 10);
+        state_warm = 0xfd; // fifth level gained
+        _warm();
+        return; // shouldn't reach this
+    }
+    // if ( (state_warm == -2) || (state_warm == -1)) return; // preserve penalty
+    state_warm = 0; // sieve fall-through
+}
+
 void filter(void) {
+
+    // capture_warm();
 
     /* stanza -- control H */
     if ((uint8_t) *buf == 8) { // Control H
@@ -87,5 +149,6 @@ void USART_0_example(void) {
         buf = (uint8_t *)tib;
         filter();
         io_write(io, (uint8_t *)tib, 1); // 1  is also length
+        capture_warm();
     }
 }
